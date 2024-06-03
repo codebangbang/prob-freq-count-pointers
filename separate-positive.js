c11b1a8c27dd5ef7e190043226ceb6dcadba6643
codebangbang <ryfair@gmail.com>
2024-06-03 15:03:45 -0400

    separate-positive passes tests

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr1) {
+    let left = 0;
+    let right = arr1.length - 1;
+    
+    while (left < right) {
+        if (arr1[left] < 0 && arr1[right] > 0) {
+            [arr1[left], arr1[right]] = [arr1[right], arr1[left]];
+        }
+        if (arr1[left] > 0) left++;
+        if (arr1[right] < 0) right--;
+    }
+    return arr1;
 }
+
+module.exports = separatePositive;
