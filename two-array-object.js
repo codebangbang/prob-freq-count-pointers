19ae12783e40cdf90c93f7e46c8f8fa102aeec82
codebangbang <ryfair@gmail.com>
2024-06-03 14:43:49 -0400

    two-array-object passes tests

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,10 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject( keys, values) {
+    let result = {};
+    for (let i = 0; i < keys.length; i++) {
+        result[keys[i]] = values[i] || null;
+    }
+    return result;
 }
+
+module.exports = twoArrayObject;
