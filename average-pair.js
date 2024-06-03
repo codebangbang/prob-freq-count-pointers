3754e68071ce2872de009f6675844af61873cb41
codebangbang <ryfair@gmail.com>
2024-06-03 14:36:03 -0400

    average-pair passes tests

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,19 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(array, targetAvg) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    let avg = (array[left] + array[right]) / 2;
+    if (avg === targetAvg) {
+      return true;
+    } else if (avg < targetAvg) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return false;
 }
+
+module.exports = averagePair;
