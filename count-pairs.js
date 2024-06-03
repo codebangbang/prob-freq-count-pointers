6135dd55f9de3392b7bc007dddfe91c67d33711e
codebangbang <ryfair@gmail.com>
2024-06-03 15:49:21 -0400

    count-pairs tests pass

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,24 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr1, num) {
+    let count = 0;
+    let left = 0;
+    let right = arr1.length - 1;
+    arr1.sort((a, b) => a - b);
+
+    while (left < right) {
+        let sum = arr1[left] + arr1[right];
+        if (sum === num) {
+            count++;
+            left++;
+            right--;
+        } else if (sum < num) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return count;
 }
+
+module.exports = countPairs;
